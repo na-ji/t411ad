b2c76494cdd9a44c163e8e697439eb95dff32a36
na-ji <naji.demolitionman@gmail.com>
2015-02-22 20:35:21 +0100

    Delete Colors functions

--- src/download.php
+++ src/download.php
@@ -6,14 +6,6 @@ header('Content-Type: text/plain; charset=utf-8');
 
 use Naji\T411\Client as T411Client;
 
-$colors = new Colors;
-
-function echoColor($text, $foreground_color = null, $background_color = null)
-{
-	global $colors;
-	echo $colors->getColoredString($text, $foreground_color, $background_color);
-}
-
 $client = new T411Client(T411_LOGIN, T411_PASSWORD, TVDB_CACHE, 'http://www.t411.io');
 $starTime = time();
 setlocale(LC_TIME, 'fr_FR.UTF8');
@@ -26,20 +18,10 @@ foreach ($tvshows as $tvshow) {
 	echo "Telechargement des episodes de ".$tvshow['tvshow']->getName()."\n";
 	echo "   ".count($tvshow['episodes'])." episode".(count($tvshow['episodes']) > 1 ? 's' : '')." à télécharger\n";
 	foreach ($tvshow['episodes'] as $episode) {
-		if ($episode->getNumber() == 16)
-		{
-			$numero = 954;
-		} elseif ($episode->getNumber() == 17)
-		{
-			$numero = 953;
-		} else {
-			$numero   = ($episode->getNumber() >= 31 ? 1057 : ($episode->getNumber() >= 9 ? 937 : 936)) + $episode->getNumber();
-		}
-		$saison   = 967 + $episode->getSeason();
 		$acronyme = 'S'.str_pad($episode->getSeason(), 2, "0", STR_PAD_LEFT).'E'.str_pad($episode->getNumber(), 2, "0", STR_PAD_LEFT);
 		
 		echo "   Saison ".$episode->getSeason()." épisode ".$episode->getNumber()." ".$acronyme."\n";
-		$liens = $client->search(array('search' => $tvshow['tvshow']->getName().' '.$acronyme/*, 'term[46][]' => $numero, 'term[45][]' => $saison*/));
+		$liens = $client->search(array('search' => $tvshow['tvshow']->getName().' '.$acronyme));
 		$s     = count($liens) > 1 ? 's' : '';
 		
 		echo "      ".count($liens)." lien".$s." trouvé".$s."\n";
@@ -47,22 +29,17 @@ foreach ($tvshows as $tvshow) {
 			$hd      = array();
 			$full_hd = array();
 			foreach ($liens as $lien) {
-				$color = 'red';
-				if (false === strpos($lien['name'], $acronyme))
+				if (false !== strpos($lien['name'], $acronyme))
 				{
-					$color = 'red';
-				} else {
 					if (strpos($lien['name'], '720p') > 0)
 					{
 						$hd[] = $lien;
-						$color = 'green';
 					} else if (strpos($lien['name'], '1080p') > 0)
 					{
 						$full_hd[] = $lien;
-						$color = 'green';
 					}
 				}
-				echoColor("         ".str_pad($lien['name'], 80)." ".$lien['seeder']."\n", $color);
+				echo "         ".str_pad($lien['name'], 80)." ".$lien['seeder']."\n";
 			}
 			echo "      Recherche du meilleur lien\n";
 			if (count($hd) > 0)
@@ -75,7 +52,7 @@ foreach ($tvshows as $tvshow) {
 						$bestLien = $lien;
 					}
 				}
-				echoColor("         ".str_pad($bestLien['name'], 80)." ".$bestLien['seeder']."\n", $color);
+				echo "         ".str_pad($bestLien['name'], 80)." ".$bestLien['seeder']."\n";
 			} elseif (count($full_hd) > 0)
 			{
 				$max = 0;
@@ -86,9 +63,9 @@ foreach ($tvshows as $tvshow) {
 						$bestLien = $lien;
 					}
 				}
-				echoColor("         ".str_pad($bestLien['name'], 80)." ".$bestLien['seeder']."\n", "green");
+				echo "         ".str_pad($bestLien['name'], 80)." ".$bestLien['seeder']."\n";
 			} else {
-				echoColor("      Aucun lien en qualité suffisante\n", null, "red");
+				echo "      Aucun lien en qualité suffisante\n";
 				break;
 			}
 
@@ -104,4 +81,4 @@ foreach ($tvshows as $tvshow) {
 }
 
 $em->flush();
-echo "Fin du script. Temps total mis : ".(time()-$starTime)."s\n";
+echo "Fin du script. Temps total mis : ".(time()-$starTime)."s\n\n";
